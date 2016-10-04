0aa96f03a949c847a8bcafe081b48f908b327b25
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2016-10-04 18:14:31 -0400

    Refactored tests to use Game as a field

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -1,5 +1,6 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -7,10 +8,15 @@ import static org.junit.Assert.assertThat;
 
 public class GameTest {
 
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
     @Test
     public void GameByDefault_puts_50_questions_in_each_list() {
-        Game game = new Game();
-
         assertThat(game.popQuestions.size(), is(50));
         assertThat(game.scienceQuestions.size(), is(50));
         assertThat(game.sportsQuestions.size(), is(50));
@@ -19,8 +25,6 @@ public class GameTest {
 
     @Test
     public void GameByDefault_puts_proper_questions() {
-        Game game = new Game();
-
         assertThat(game.popQuestions.get(0).toString(), is("Pop Question 0"));
         assertThat(game.scienceQuestions.get(1).toString(), is("Science Question 1"));
         assertThat(game.sportsQuestions.get(2).toString(), is("Sports Question 2"));
@@ -29,14 +33,12 @@ public class GameTest {
 
     @Test
     public void GameByDefault_has_zero_players(){
-        Game game = new Game();
         assertThat(game.players.size(), is(0));
         assertThat(game.currentPlayer, is(0));
     }
 
     @Test
     public void Add_GivenBob_willGiveGameOnePlayer() {
-        Game game = new Game();
         game.add("Bob");
         assertThat(game.players.size(), is(1));
     }
